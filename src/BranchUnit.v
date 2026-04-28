0da6653dd3f634072328420b7d072784b999b4fc
penchel <penchel.andre@gmail.com>
2026-04-27 21:58:32 -0300

    BEQ implementado

--- src/BranchUnit.v
+++ src/BranchUnit.v
@@ -27,7 +27,10 @@ module BranchUnit (
         branch_taken  = 1'b0;
         branch_target = pc_ex + 32'd4;
 
-        //TODO: Implementar lógica do BEQ aqui!!!
+        if (opcode == BEQ && rs1_value == rs2_value) begin
+            branch_taken  = 1'b1;
+            branch_target = pc_ex + branch_imm;
+        end
     end
 
 endmodule
